f39bfa8b6e4937510665d21034f4e3878830c32e
LawrenceTSM <lawrence.mouarkach@moneysupermarket.com>
2017-11-20 17:34:07 +0000

    change credentials

--- src/main/java/com/lawrencemouarkach/CallHelloLambda.java
+++ src/main/java/com/lawrencemouarkach/CallHelloLambda.java
@@ -1,5 +1,6 @@
 package com.lawrencemouarkach;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
 
@@ -9,7 +10,10 @@ public class CallHelloLambda {
     private static final int AGE = 26;
 
     private static final HelloLambdaService HELLO_LAMBDA_SERVICE = LambdaInvokerFactory.builder()
-            .lambdaClient(AWSLambdaClientBuilder.standard().withRegion("eu-west-2").build())
+            .lambdaClient(AWSLambdaClientBuilder.standard()
+                    .withRegion("eu-west-2")
+                    .withCredentials(new ProfileCredentialsProvider("adminuser"))
+                    .build())
             .build(HelloLambdaService.class);
 
     private static String retriveHelloName() {
