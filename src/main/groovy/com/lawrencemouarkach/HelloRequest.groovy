5140215dbb622fbc065681b0b596f710a1c2353e
LawrenceTSM <lawrence.mouarkach@moneysupermarket.com>
2017-11-20 16:41:00 +0000

    add constructor

--- src/main/groovy/com/lawrencemouarkach/HelloRequest.groovy
+++ src/main/groovy/com/lawrencemouarkach/HelloRequest.groovy
@@ -4,6 +4,11 @@ import groovy.transform.Canonical
 
 @Canonical
 class HelloRequest {
-    String name;
-    int age;
+    String name
+    int age
+
+    HelloRequest(String name, int age) {
+        this.name = name
+        this.age = age
+    }
 }
