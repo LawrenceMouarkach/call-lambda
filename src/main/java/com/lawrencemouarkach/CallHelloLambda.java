78f72933f7826dc5a1d8994c212e94ace59f040f
LawrenceTSM <lawrence.mouarkach@moneysupermarket.com>
2017-11-20 15:40:38 +0000

    inline lambdaClient

--- src/main/java/com/lawrencemouarkach/CallHelloLambda.java
+++ src/main/java/com/lawrencemouarkach/CallHelloLambda.java
@@ -6,8 +6,7 @@ import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
 public class CallHelloLambda {
 
     private static final HelloLambdaService HELLO_LAMBDA_SERVICE = LambdaInvokerFactory.builder()
-            .lambdaClient(AWSLambdaClientBuilder.standard()
-                    .withRegion("eu-west-2").build())
+            .lambdaClient(AWSLambdaClientBuilder.standard().withRegion("eu-west-2").build())
             .build(HelloLambdaService.class);
 
     private static String retriveHelloName() {
